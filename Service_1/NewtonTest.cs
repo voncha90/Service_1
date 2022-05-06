a688e13fd94cd5b71abe1704762866ae7a202330
voncha90 <voncha1990@gmail.com>
2022-05-06 10:01:09 -0500

    Added a class

---
+++ Service_1/NewtonTest.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Service_1
+{
+    public class Service1NewtonTest
+    {
+        public  void Serialize(object obj)
+        {
+            const string filePath = @"C:\Users\vonch\Desktop\json.txt";
+            var serializer = new JsonSerializer();
+
+            using (var sw = new StreamWriter(filePath))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, obj);
+            }
+        }
+    }
+}
--- Service_1/Service_1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Service_1
-{
-    public class Service_1
-    {
-    }
-}
