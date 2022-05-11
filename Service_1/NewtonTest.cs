16da282ab6c3e0e26b99b12babe597ca44689842
voncha90 <voncha1990@gmail.com>
2022-05-11 01:43:13 -0500

    commit 2

--- Service_1/NewtonTest.cs
+++ Service_1/NewtonTest.cs
@@ -6,18 +6,35 @@ using System.Text;
 
 namespace Service_1
 {
-    public class Service1NewtonTest
+    public class ServiceAClass
     {
-        public  void Serialize(object obj)
-        {
-            const string filePath = @"C:\Users\vonch\Desktop\json.txt";
-            var serializer = new JsonSerializer();
 
-            using (var sw = new StreamWriter(filePath))
-            using (JsonWriter writer = new JsonTextWriter(sw))
+        public string GetServiceAClassData()
+        {
+            string result = string.Empty;
+            Account account = new Account
             {
-                serializer.Serialize(writer, obj);
-            }
+                Email = "james@example.com",
+                Active = true,
+                CreatedDate = new DateTime(2013, 1, 20, 0, 0, 0, DateTimeKind.Utc),
+                Roles = new List<string>
+    {
+        "User",
+        "Admin"
+    }
+            };
+
+            result = JsonConvert.SerializeObject(account, Formatting.Indented);
+
+            return result;
         }
+    }
+
+    public class Account
+    {
+        public string Email { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public IList<string> Roles { get; set; }
     }
 }
